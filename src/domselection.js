1951d0b486ade37c5658f8cf67c60fc3970d3e04
Royal2603 <royalllll2603@gmail.com>
2024-11-13 14:38:01 +0000

    implement random logic

---
+++ src/domselection.js
@@ -0,0 +1,5 @@
+const yearEl = document.querySelector(".year");
+const inputEl = document.querySelector("input");
+const formEl = document.querySelector("form");
+
+export { yearEl, inputEl, formEl };
--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
 import "./index.css";
+import { yearEl, inputEl, formEl } from "./domselection";
+const tasks = [];
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (!inputEl.value) {
+    return;
+  }
+  tasks.push({
+    title: inputEl.value,
+    iscompleted: false,
+    id: crypto.randomUUID(),
+  });
+  console.log(tasks);
+
+  inputEl.value = "";
+});
+// yearEl.textContent = `${new Date().getFullYear()}`;
+
+(function renderDate() {
+  const year = new Date().getFullYear();
+  yearEl.textContent = `${year}`;
+})();
